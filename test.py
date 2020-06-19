72d5792d663e1c0c892629c3bcc6e552d200e255
rajanarasimhan <rajanarasimhan1811@gmail.com>
2020-06-19 18:17:56 +0530

    changed the weights path and corrected __init__.py mistake and fixed tqdm problem

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 from Badminton import Detector
 
 obj = Detector(tiny=True)
-obj.get_heatmap("Badminton/images/video2.mp4",optimization = True)
+obj.detect_players_video("Badminton/images/video2.mp4",optimization = True, frames_skipped_input=5)
